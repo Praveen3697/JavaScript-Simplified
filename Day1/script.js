ef2a39037264bb76d4f94ea0ff108c467d7b704b
Praveen3697 <nairpraveen1997@gmail.com>
2022-05-16 14:47:51 +0530

    Day 1

---
+++ Day1/script.js
@@ -0,0 +1,37 @@
+console.log("Hello world!!")
+
+let favouriteNumber = 3
+/*
+if you want to update the value of favouriteNumber then following is the step
+*/
+favouriteNumber = 4
+console.log(favouriteNumber)
+
+let a = 1
+let b = 2
+let c = 3
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// a = 3
+
+// console.log(a)
+
+// // I can also assign one variable to other for example
+// b = 15
+
+// let d = b
+// console.log(d)
+
+console.log((a+b)/c)
+
+let name = "Praveen"
+
+console.log("Hello my name is " + name)
+
+let happy = true
+let fun = true
+
+console.log(happy && fun)
